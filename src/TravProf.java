ea86e40b401afb61ec5b4741dfcf0fd2333adb62
Amy Richards <anthropofobe@gmail.com>
2009-09-23 16:42:26 -0400

    Initial Uploads

---
+++ src/TravProf.java
@@ -0,0 +1,8 @@
+
+public class TravProf {
+
+	public TravProf() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/TravProfDB.java
@@ -0,0 +1,19 @@
+
+public class TravProfDB {
+
+	public TravProfDB(String test) {
+		// TODO Auto-generated constructor stub
+	}
+	public void insertNewProfile(TravProf test){}
+	public boolean deleteProfile(String test1, String test2){return true;}
+	public TravProf findProfile(String test1, String test2){return new TravProf();}
+	public TravProf findFirstProfile() {return new TravProf();}
+	public TravProf findNextProfile() {return new TravProf();}
+	public void writeAllTravProf(String test1){}
+	public void initializeDataBase(String test){}
+	
+	
+	
+	
+
+}
